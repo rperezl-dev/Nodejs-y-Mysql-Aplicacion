089def113da4dc64ece62b312f9dd2b4bab6e5b5
Reinaldo Pérez <85711442+rperezl-dev@users.noreply.github.com>
2022-03-06 16:55:14 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  database: {
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'db_links'
+  }
+};
---
+++ src/lib/handlebars.js
@@ -0,0 +1,8 @@
+const timeago = require('timeago.js');
+const timeagoInstance = timeago();
+const helpers = {};
+
+helpers.timeago = (savedTimestamp) => {
+  return timeagoInstance.format(savedTimestamp);
+};
+module.exports = helpers;
